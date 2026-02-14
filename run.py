6177e245b33c1d97d68518aaa44fa805bdaed2d7
Yash Prajapati <yashypsoft@gmail.com>
2026-02-14 18:02:34 +0530

    gold deal scranner code updated added ui pages as well

--- run.py
+++ run.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "api:app",
         host="0.0.0.0",
-        port=8000,
+        port=8001,
         reload=True,
         log_level="info"
     )
